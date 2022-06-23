13a1bdaddd9f5917531826064d33d7b7e3f251bb
aidan-qa <aidan.aedy@qa.com>
2022-06-23 12:06:48 +0100

    added the test files

---
+++ testing.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def menu():
+    print("This is the menu method")
+
+def main():
+
+
+    print()
+    print("This is the main method")
+    print("            -------------------------    ")
+
+    menu()
+
+    # fileout section needs to add back in later (number_entries)
+
+
+# ***************************   END MAIN FUNC. ! **************************
+
+
+if __name__ == '__main__':
+    main()
+    menu()
+
